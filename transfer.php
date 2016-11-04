7d7cfe040f9e2bb91f9eea920cc8babde983f1c3
Opeyemi Ibrahim <sirkhadrealryan@gmail.com>
2016-11-04 17:04:07 +0100

    Create transfer.php

---
+++ transfer.php
@@ -0,0 +1,24 @@
+<?php
+set_time_limit(0); //Unlimited max execution time
+ 
+$path = 'folder/store.zip'; //The Folder you are saving the upload file to and the name 
+$url = 'http://23.229.175.226/~ckdigitalnet/_suspended/sturdyshelter.zip'; //Url where you will be downloading from
+$newfname = $path;
+echo 'Starting Download!<br>';
+$file = fopen ($url, "rb");
+if($file) {
+	$newf = fopen ($newfname, "wb");
+	if($newf)
+		while(!feof($file)) {
+			fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 );
+			echo '1 MB File Chunk Written!<br>';
+		}
+}
+if($file) {
+	fclose($file);
+}
+if($newf) {
+	fclose($newf);
+}
+echo 'Finished!';
+?>
