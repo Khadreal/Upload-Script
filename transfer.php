fa749d721e7dd9cc7b606a5a779d07fe40e98e6f
Opeyemi Ibrahim <sirkhadrealryan@gmail.com>
2018-03-17 23:48:14 +0100

    Fix typo

--- transfer.php
+++ transfer.php
@@ -2,7 +2,7 @@
 set_time_limit(0); //Unlimited max execution time
  
 $path = 'folder/store.zip'; //The Folder you are saving the upload file to and the name 
-$url = 'http://23.229.175.226/~ckdigitalnet/_suspended/sturdyshelter.zip'; //Url where you will be downloading from
+$url = 'http://example.com/site.zip'; //Url where you will be downloading from
 $newfname = $path;
 echo 'Starting Download!<br>';
 $file = fopen ($url, "rb");
